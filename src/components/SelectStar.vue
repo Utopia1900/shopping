d356b0e87658f88b7f0530ae513017aae51afbff
love_ru1900 <cosem_520@163.com>
2019-06-25 09:46:54 +0800

    customize selectStart component

---
+++ src/components/SelectStar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="stars_container">
+    <span v-for="(item,index) in stars" :key="index" @click="changeColor(item, index)"
+          :class="{active: currentIndex !=null && index <= currentIndex}">{{item.text}}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "SelectStar",
+    data() {
+      return {
+        stars: [
+          {text: '☆', selected: false},
+          {text: '☆', selected: false},
+          {text: '☆', selected: false},
+          {text: '☆', selected: false},
+          {text: '☆', selected: false}
+        ],
+        currentIndex: null
+      }
+
+    },
+    methods: {
+      changeColor(arg, index) {
+        this.currentIndex = index
+        arg.selected = !arg.selected;
+      }
+    }
+
+  }
+</script>
+
+<style>
+  .stars_container {
+    margin-top: 50px;
+    display: flex;
+    flex-direction: row;
+  }
+
+  .stars_container span {
+    height: 30px;
+    width: 30px;
+    text-align: center;
+    line-height: 30px;
+    font-size: 20px;
+    display: inline-block;
+  }
+
+  .active {
+    color: red
+  }
+</style>
--- src/views/myLower/MyLower.vue
+++ src/views/myLower/MyLower.vue
@@ -1,24 +1,34 @@
 <template>
   <div>
     <header-nav :headerNavTitle="headerNavTitle" @on-back="goBack"/>
+    <select-stars ref="select1"></select-stars>
+    <select-stars></select-stars>
+    <select-stars></select-stars>
+    <select-stars></select-stars>
+    <button @click="getSelectValue">click</button>
   </div>
 </template>
 
 <script>
   import HeaderNav from '../../components/HeaderNav'
+  import SelectStars from '../../components/SelectStar'
   export default {
     name: "MyLower",
-    data(){
+    data() {
       return {
         headerNavTitle: '我的推广'
       }
     },
     components: {
-      HeaderNav
+      HeaderNav,
+      SelectStars
     },
     methods: {
-      goBack(){
+      goBack() {
         this.$router.go(-1)
+      },
+      getSelectValue(){
+        console.log(this.$refs.select1.currentIndex + 1);
       }
     }
   }
